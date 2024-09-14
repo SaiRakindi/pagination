c67f64a384ff3a00c8c3bec08953cfe8d5f67f6d
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-14 22:51:01 +0530

    feat: products fetched and rendered on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
   const fetchProducts = async () => {
     const response = await fetch("https://dummyjson.com/products?limit=100");
     const data = await response.json();
 
-    console.log("data", data);
+    if (data && data.products) setProducts(data.products);
   };
+
+  console.log(products);
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  return <div>Pagination</div>;
+  return (
+    <div>
+      {products.length > 0 && (
+        <ul className="products">
+          {products.map((product) => (
+            <li className="products-item" key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <span>{product.title}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
