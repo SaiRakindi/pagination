f0a1b17fda9a4ce3a0a5f8068975692b3407cf77
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-14 23:19:58 +0530

    feat: paginatoin functionality on clicking buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
     const response = await fetch("https://dummyjson.com/products?limit=100");
@@ -11,23 +12,57 @@ function App() {
     if (data && data.products) setProducts(data.products);
   };
 
-  console.log(products);
-
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const selectPageHandler = (selectedPage) => {
+    setPage(selectedPage);
+  };
 
   return (
     <div>
       {products.length > 0 && (
         <ul className="products">
-          {products.map((product) => (
+          {products.slice(page * 10 - 10, page * 10).map((product) => (
             <li className="products-item" key={product.id}>
               <img src={product.thumbnail} alt={product.title} />
               <span>{product.title}</span>
             </li>
           ))}
         </ul>
+      )}
+
+      {products.length > 0 && (
+        <div className="pagination">
+          <button
+            onClick={() => {
+              if (page > 1) selectPageHandler(page - 1);
+            }}
+            disabled={page === 1}
+          >
+            ◀
+          </button>
+          {[...Array(products.length / 10)].map((_, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => selectPageHandler(index + 1)}
+                disabled={page === index + 1}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+          <button
+            disabled={page === 10}
+            onClick={() => {
+              if (page < products.length / 10) selectPageHandler(page + 1);
+            }}
+          >
+            ▶
+          </button>
+        </div>
       )}
     </div>
   );
