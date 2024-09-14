362771f797d2ec9c86ae1714d7a21d6c406e0944
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-14 22:36:01 +0530

    feat: fetch products using dummy api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 function App() {
+  const fetchProducts = async () => {
+    const response = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await response.json();
+
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return <div>Pagination</div>;
 }
 
